25099c352517c45173feea2bc08f3a0504bd4942
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-15 11:15:41 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return 0
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %1u bytes(s)\n", sizeof(char));
+printf("Size of an int: %1u bytes(s)\n", sizeof(int));
+printf("Size of a long int: %1u bytes(s)\n", sizeof(long int));
+printf("Size of a long long int: %1u bytes(s)\n", sizeof(long long int));
+printf("Size of float: %1u bytes(s)\n, sizeof(float));
+return (0);
+}
