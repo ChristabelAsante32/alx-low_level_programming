76c098279b6ccfa258006c941655ac377efc8883
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-15 14:25:35 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
-#include <unistd.h>
+#include <string.h>
 /**
- * main - C program that prints a line to the stanadard error
- * Return 1 (Success)
+ * main - Entry point
+ *
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (0);
+fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (1);
 }
