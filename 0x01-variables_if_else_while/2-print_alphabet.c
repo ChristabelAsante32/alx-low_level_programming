3d5e69a379fb1f46bb60152e60cdfb9bfe42aeae
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-18 07:16:48 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return Always 0 sucess
+ */
+int main
+{
+	char c;
+	for (c = 'a' ; 'c' <= 'z' ; c++)
+	{
+		putchar (c);
+	}
+	putchar ('\n');
+	return (0);
+}
