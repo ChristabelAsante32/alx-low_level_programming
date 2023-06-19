59cc4701ced14e96b3f8f947f8b85183c9a05544
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-19 06:54:46 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*main - Entry point
+*Return: Always 0 (success)
+*/
+
+int main(void)
+{
+	int i = 0;
+	
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
