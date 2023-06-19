d94e384b8939469bc8d33c14913f20ae4eb68944
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-19 07:13:31 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
- * main - Entry point
- * Return: Always 0 (success)
- */
+*main - Entry point
+*
+*Return: 0 on success
+*/
+
 int main(void)
 {
-	int ch = 'a';
+	int ch1 = 'a', ch2 = 'A';
 
-		while (ch <= 'z')
-		{
-			if (ch != 'q' && ch != 'e')
-			putchar(ch);
-			ch++;
-		}
+	while (ch1 <= 'z')
+	{
+		putchar(ch1);
+		ch1++;
+	}
+	while (ch2 <= 'Z')
+	{
+		putchar(ch2);
+		ch2++;
+	}
 	putchar('\n');
 	return (0);
 }
