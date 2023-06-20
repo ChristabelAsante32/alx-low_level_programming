8c885b1256db229a06f2499589406db022a95c0a
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-20 15:13:32 -0700

    put

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet-in lowercase, followed by a new line
+ *
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	char alph = 'a';
+
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+		alph++;
+	}
+	 _putchar('\n');
+}
+
