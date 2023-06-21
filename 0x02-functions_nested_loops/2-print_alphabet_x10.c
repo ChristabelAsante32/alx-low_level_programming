8f5050811d63883c173860235f417516828100ef
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-21 00:45:11 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - This function
+ * print_alphabet_x10 - Prints the alphabet in lowercase 10 times
+ */
+void print_alphabet_x10(void)
+{
+	char alph = 'a';
+	int n;
+
+	for (n = 0; n <= 10; n++)
+		while (alph <= 'z')
+		{
+			_putchar(alph);
+			       alph++;
+		}
+	_putchar ('\n');
+}
