f12dbd84add696c9a0767ac6b6822008ce55a677
Christabel Asante <asanteachristabel32@gmail.com>
2023-06-24 07:25:39 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,15 +7,15 @@
  */
 void print_triangle(int size)
 {
-	int c, d;
+	int c, d, e;
 
 	if (size > 0)
 	{
 		for (c = 1; c <= size; c++)
 		{
-			for (d = size - c; d = 0; d--)
+			for (d = size; d <= size; d++)
 				_putchar(' ');
-			for (d = 0; d < c; d++)
+			for (e = 1; e <= c; e++)
 				_putchar('#');
 			if (c == size)
 				continue;
